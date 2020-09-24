3a847b67cd5039bb51676e81702ce98f9127ce9b
consultorsandro <68569797+consultorsandro@users.noreply.github.com>
2020-09-23 21:46:07 -0300

    Aula 18

---
+++ relacionais.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Strings:", "Banana" == "Banana")
+	fmt.Println("!=", 3 != 2)
+	fmt.Println("<", 3 < 2)
+	fmt.Println(">", 3 > 2)
+	fmt.Println("<=", 3 <= 2)
+	fmt.Println(">=", 3 >= 2)
+
+	d1 := time.Unix(0, 0)
+	d2 := time.Unix(0, 0)
+	fmt.Println("Datas: ", d1 == d2)
+	fmt.Println("Datas: ", d1.Equal(d2)) // preferencialmente use a função Equal quando comparar tempo
+	fmt.Print(d1)
+
+}
