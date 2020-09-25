9443bfdfa6e0df8b033aacc1150ab54908a61ac9
consultorsandro <68569797+consultorsandro@users.noreply.github.com>
2020-09-25 16:49:37 -0300

    Aula 60

---
+++ composicao.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type esportivo interface {
+	ligarTurbo()
+}
+
+type luxuoso interface {
+	fazerBaliza()
+}
+
+type esportivoLuxuoso interface {
+	esportivo
+	luxuoso
+}
+type bmw7 struct{}
+
+func (b bmw7) ligarTurbo() {
+	fmt.Println("Turbo...")
+}
+
+func (b bmw7) fazerBaliza() {
+	fmt.Println("Baliza...")
+}
+
+func main() {
+	var fusca esportivoLuxuoso = bmw7{}
+	fusca.ligarTurbo()
+	fusca.fazerBaliza()
+}
